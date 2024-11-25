7179d0ce652fe1475b171f8745e5e415f447bb84
manikjain105 <115030365+manikjain105@users.noreply.github.com>
2024-11-25 19:16:46 +0530

    Update server_broad1.c

--- server_broad1.c
+++ server_broad1.c
@@ -1,38 +1,46 @@
- #include <stdio.h>
- #include <stdlib.h>
- #include <unistd.h>
- #include <sys/types.h>
- #include <sys/socket.h>
- #include <arpa/inet.h>
- #include <netinet/in.h>
- #include <string.h>
- #define  err_log(log) do{perror(log); exit(1);}while(0)
- #define  N  128
- int main(int argc, const char *argv[])
- {
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BROADCAST_IP "127.0.0.255"  // Broadcast IP for the network segment
+#define PORT 22000                    // Port number for broadcasting
+#define MESSAGE "Broadcast message"    // Message to broadcast
+
+int main() {
     int sockfd;
-    char buf[N];
-    struct sockaddr_in broadcastaddr, srcaddr;
-    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        err_log("fail to socket");
+    struct sockaddr_in broadcastAddr;
+    int broadcastPermission = 1;  // Allow broadcast
+
+    // Create socket for sending datagrams
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket failed");
+        exit(1);
     }
-    broadcastaddr.sin_family = AF_INET;
-    broadcastaddr.sin_addr.s_addr = inet_addr("192.168.1.255");   
-                                                                                       //Broadcast address/ INADDR_ANY
-    broadcastaddr.sin_port = htons(10000);
-    if(bind(sockfd, (struct sockaddr*)&broadcastaddr, sizeof(broadcastaddr)) < 0)
-    {
-        err_log("fail to bind");
+
+    // Set socket options to allow broadcast
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastPermission, sizeof(broadcastPermission)) < 0) {
+        perror("setsockopt failed");
+        exit(1);
     }
-    socklen_t addrlen = sizeof(struct sockaddr);
-    while(1)
-    {
-        if(recvfrom(sockfd,buf, N, 0, (struct sockaddr *)&srcaddr, &addrlen) < 0)
-        {
-            err_log("fail to sendto");
-        }
-        printf("buf:%s ---> %s %d\n", buf, inet_ntoa(srcaddr.sin_addr), ntohs(srcaddr.sin_port));
+
+    // Setup the broadcast address structure
+    memset(&broadcastAddr, 0, sizeof(broadcastAddr));
+    broadcastAddr.sin_family = AF_INET;
+    broadcastAddr.sin_addr.s_addr = inet_addr(BROADCAST_IP);
+    broadcastAddr.sin_port = htons(PORT);
+
+    // Broadcast the message
+    if (sendto(sockfd, MESSAGE, strlen(MESSAGE), 0, (struct sockaddr *)&broadcastAddr, sizeof(broadcastAddr)) < 0) {
+        perror("sendto failed");
+        exit(1);
     }
+
+    printf("Broadcast message sent: %s\n", MESSAGE);
+
+    // Close the socket
+    close(sockfd);
     return 0;
- }
+}
